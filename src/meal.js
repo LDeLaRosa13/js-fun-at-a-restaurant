f800e82b3742f8824cbfc6e8e3155a8b3c6517ca
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-05-10 18:37:21 -0400

    complete meal file

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItem) {
+  return `Delicious ${menuItem}`
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(topping, ingredients) {
+  if(!ingredients.includes(topping)) {
+    ingredients.push(topping)
+  }
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice(oldPrice) {
+  oldPrice *= .9
+  return oldPrice
+}
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
