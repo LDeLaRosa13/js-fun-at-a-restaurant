06587edd26d79f17de4dc5acc77135a0b0f32187
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-05-11 22:27:20 -0400

    Update test files

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,28 @@
+function createRestaurant(name) {
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+
+}
+
+function addMenuItem(restaurant, meal) {
+  console.log(restaurant)
+  console.log(meal)
+  // if (restaurant.menus === meal.type) {
+    return restaurant.menus.lunch.push(meal)
+  }
+//  } 
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant, 
+  addMenuItem,
   // removeMenuItem,
   // checkForFood
 }
